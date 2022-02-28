0eae399dcaa4f858ea2321630e0c57055ae3467b
Joseph McGregor <joseph00@my.yorku.ca>
2022-02-28 07:09:01 -0500

    send props to appointment component + dummy data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,50 @@
 import React, { useState } from "react";
 
+
+
 import "components/Application.scss";
 
 import { DayList } from "./DayList";
+import Appointment from "./Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -34,7 +76,11 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu"><DayList
+          days={days}
+          day={"Monday"}
+          setDay={setDay}
+        /></nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -42,12 +88,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        <DayList
-          days={days}
-          day={"Monday"}
-          setDay={setDay}
-        />
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
